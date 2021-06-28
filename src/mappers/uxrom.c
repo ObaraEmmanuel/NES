487ca62da24f24d95cbe55c30523a11ff653cb75
ObaraEmmanuel <emmanuelobarany@gmail.com>
2021-06-28 13:22:22 +0300

    Use generic implementation in UxROM

--- src/mappers/uxrom.c
+++ src/mappers/uxrom.c
@@ -12,8 +12,6 @@ static void write_CHR(Mapper*, uint16_t, uint8_t);
 void load_UXROM(Mapper* mapper){
     mapper->read_PRG = read_PRG;
     mapper->write_PRG = write_PRG;
-    mapper->read_CHR = read_CHR;
-    mapper->write_CHR = write_CHR;
 
     if(!mapper->CHR_banks){
         mapper->CHR_RAM = malloc(0x2000);
@@ -32,18 +30,4 @@ static uint8_t read_PRG(Mapper* mapper, uint16_t address){
 
 static void write_PRG(Mapper* mapper, uint16_t address, uint8_t value){
     mapper->bank_select = value;
-}
-
-
-static uint8_t read_CHR(Mapper* mapper, uint16_t address){
-    return mapper->CHR_RAM[address];
-}
-
-
-static void write_CHR(Mapper* mapper, uint16_t address, uint8_t value){
-    if(mapper->CHR_banks){
-        LOG(DEBUG, "Attempted to write to CHR-ROM");
-        return;
-    }
-    mapper->CHR_RAM[address] = value;
 }
