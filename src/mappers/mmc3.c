0393bedb421f0db2223ed4a33265dff63b94ec56
ObaraEmmanuel <emmanuelobarany@gmail.com>
2026-08-19 21:00:32 +0300

    Implement common mmc3 submappers

--- src/mappers/mmc3.c
+++ src/mappers/mmc3.c
@@ -17,11 +17,17 @@ typedef struct {
     uint8_t IRQ_counter;
     uint8_t IRQ_cleared;
     uint8_t IRQ_enabled;
+    uint8_t alt_clocking;
+    uint8_t RAM_enabled;
+    uint8_t RAM_protect;
     uint32_t PRG_clamp;
     uint32_t CHR_clamp;
     size_t cycles;
 } MMC3_t;
 
+static uint8_t read_ROM_MMC6(Mapper *mapper, uint16_t address);
+
+static void write_ROM_MMC6(Mapper *mapper, uint16_t address, uint8_t value);
 
 static uint8_t read_PRG(Mapper * mapper, uint16_t addr);
 
@@ -34,6 +40,8 @@ static void write_bank_data(Mapper *mapper, uint8_t val);
 static void on_filtered_a12(Mapper* mapper);
 
 static void set_bus(Mapper * mapper, uint16_t addr);
+
+static void set_bus_mc_acc(Mapper * mapper, uint16_t addr);
 
 
 int load_MMC3(Mapper *mapper) {
@@ -63,6 +71,20 @@ int load_MMC3(Mapper *mapper) {
         mmc3->CHR_bank_ptrs[i] = mapper->CHR_ROM;
     mmc3->CHR_bank_ptrs[1] = mmc3->CHR_bank_ptrs[0] + 0x400;
     mmc3->CHR_bank_ptrs[3] = mmc3->CHR_bank_ptrs[0] + 0x400;
+
+    if (mapper->submapper == 1) {
+        // MMC6: enable full write protection
+        mapper->write_ROM = write_ROM_MMC6;
+        mapper->read_ROM  = read_ROM_MMC6;
+    } else if (mapper->submapper == 3) {
+        // use MC-ACC clocking behaviour
+        mapper->set_bus = set_bus_mc_acc;
+    } else if (mapper->submapper == 4) {
+        // NEC uses alternate clocking
+        mmc3->alt_clocking = 1;
+    } else if (mapper->submapper == 5) {
+        LOG(WARN, "T9552 de-scrambling required");
+    }
     return 0;
 }
 
@@ -79,19 +101,99 @@ static void set_bus(Mapper *mapper, uint16_t addr) {
     mmc3->a12 = a12;
 }
 
+static void set_bus_mc_acc(Mapper *mapper, uint16_t addr) {
+    MMC3_t *mmc3 = mapper->extension;
+    uint8_t a12 = (addr & 0x1000) > 0;
+    if (mmc3->a12 > a12) {
+        mmc3->cycles++;
+        if (mmc3->cycles == 1) {
+            // clock counter
+            on_filtered_a12(mapper);
+        } else if (mmc3->cycles == 8) {
+            mmc3->cycles = 0;
+        }
+    }
+    mmc3->a12 = a12;
+}
+
 static void on_filtered_a12(Mapper* mapper) {
     MMC3_t *mmc3 = mapper->extension;
-
-    if(mmc3->IRQ_cleared || !mmc3->IRQ_counter) {
+    uint8_t counter = mmc3->IRQ_counter;
+    if (mmc3->IRQ_cleared || !mmc3->IRQ_counter)
         mmc3->IRQ_counter = mmc3->IRQ_latch;
-        mmc3->IRQ_cleared = 0;
-    }else {
+    else
         mmc3->IRQ_counter--;
+
+    if (mmc3->alt_clocking) {
+        if ((counter > 0 || mmc3->IRQ_cleared) && !mmc3->IRQ_counter && mmc3->IRQ_enabled)
+            interrupt(&mapper->emulator->cpu, MAPPER_IRQ);
+    } else if (!mmc3->IRQ_counter && mmc3->IRQ_enabled)
+        interrupt(&mapper->emulator->cpu, MAPPER_IRQ);
+
+    mmc3->IRQ_cleared = 0;
+}
+
+static uint8_t read_ROM_MMC6(Mapper *mapper, uint16_t address) {
+    if (address < 0x6000) {
+        // expansion rom
+        LOG(DEBUG, "Attempted to read from unavailable expansion ROM");
+        return mapper->emulator->mem.bus;
+    }
+    if (address < 0x8000) {
+        // PRG ram
+        if (mapper->PRG_RAM == NULL) {
+            LOG(DEBUG, "Attempted to read from non existent PRG RAM");
+            return mapper->emulator->mem.bus;
+        }
+        MMC3_t *mmc3 = mapper->extension;
+        // no bank is enabled so 0x7000 - 0x7fff is open bus
+        if ((mmc3->RAM_protect & 0xA0) == 0)
+            return mapper->emulator->mem.bus;
+
+        // at least one bank is enabled, so disabled bank returns 0 and not open bus
+        uint16_t ram_addr = address - 0x6000 & mapper->PRG_RAM_clamp;
+        if (ram_addr < 0x7200) {
+            if (mmc3->RAM_protect & BIT_5)
+                return mapper->PRG_RAM[ram_addr];
+            return 0;
+        }
+
+        if (mmc3->RAM_protect & BIT_7)
+            return mapper->PRG_RAM[ram_addr];
+        return 0;
     }
 
-    if (!mmc3->IRQ_counter && mmc3->IRQ_enabled) {
-        interrupt(&mapper->emulator->cpu, MAPPER_IRQ);
+    // PRG
+    return mapper->read_PRG(mapper, address);
+}
+
+static void write_ROM_MMC6(Mapper *mapper, uint16_t address, uint8_t value) {
+    if (address < 0x6000) {
+        LOG(DEBUG, "Attempted to write to unavailable expansion ROM");
+        return;
     }
+
+    if (address < 0x8000) {
+        // extended ram
+        if (mapper->PRG_RAM == NULL) {
+            LOG(DEBUG, "Attempted to write to non existent PRG RAM");
+            return;
+        }
+        MMC3_t *mmc3 = mapper->extension;
+        uint16_t ram_addr = address - 0x6000 & mapper->PRG_RAM_clamp;
+        // if writing is enabled but reading is not for that bank,
+        // then the writing can't go through
+        if (ram_addr < 0x7200) {
+            if ((mmc3->RAM_protect & 0x30) == 0x30)
+                mapper->PRG_RAM[ram_addr] = value;
+        } else if ((mmc3->RAM_protect & 0xC0) == 0xC0) {
+            mapper->PRG_RAM[ram_addr] = value;
+        }
+        return;
+    }
+
+    // PRG
+    mapper->write_PRG(mapper, address, value);
 }
 
 uint8_t read_PRG(Mapper *mapper, uint16_t addr) {
@@ -131,6 +233,9 @@ void write_PRG(Mapper *mapper, uint16_t addr, uint8_t val) {
             mmc3->next_bank_data = val & 0x7;
             mmc3->PRG_mode = (val >> 6) & 1;
             mmc3->CHR_inversion = (val >> 7) & 1;
+            mmc3->RAM_enabled = (val >> 5) & 1;
+            if (!mmc3->RAM_enabled)
+                mmc3->RAM_protect = 0;
             break;
         case 0x8001:
             write_bank_data(mapper, val);
@@ -140,14 +245,20 @@ void write_PRG(Mapper *mapper, uint16_t addr, uint8_t val) {
                 set_mirroring(mapper, val & 1 ? HORIZONTAL : VERTICAL);
             break;
         case 0xA001:
-            // PRG RAM write protect stuff
+            if (mmc3->RAM_enabled)
+                mmc3->RAM_protect = val;
             break;
         case 0xC000:
             mmc3->IRQ_latch = val;
+            if (mapper->submapper == 4 && !mmc3->IRQ_latch)
+                mmc3->IRQ_enabled = 0;
             break;
         case 0xC001:
-            mmc3->IRQ_counter = 0;
             mmc3->IRQ_cleared = 1;
+            if (mapper->submapper == 3) {
+                // MC-ACC resets pulse counter on $C001 write
+                mmc3->cycles = 0;
+            }
             break;
         case 0xE000:
             mmc3->IRQ_enabled = 0;
